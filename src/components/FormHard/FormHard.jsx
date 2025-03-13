096d9e7c9994f22f0631cbf19b4244ef60391cfc
Andrew <psixkrichit@gmail.com>
2025-03-14 01:29:43 +0200

    full form ready except adding contacts

--- src/components/FormHard/FormHard.jsx
+++ src/components/FormHard/FormHard.jsx
@@ -7,31 +7,71 @@ const FormHard = () => {
     contactName: "",
     contactPhoneNumber: "",
     contactCity: "",
+    contactType: "",
+    contactGender: "",
+    contactAgreement: false,
   };
   const handleSubmit = (values, options) => {
     console.log("values", values);
-    console.log("options", options);
     options.resetForm();
   };
   return (
     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      <Form className={s.Form}>
-        <label className={s.labelForm}>
-          <span>Ім'я</span>
-          <Field className={s.Field} name="contactName"></Field>
-        </label>
-        <label className={s.labelForm}>
-          <span>Телефон</span>
-          <Field className={s.Field} name="contactPhoneNumber"></Field>
-        </label>
-        <label className={s.labelForm}>
-          <span>Город</span>
-          <Field className={s.Field} name="contactCity"></Field>
-        </label>
-        <button type="submit" className={s.ButtonAdd}>
-          Add
-        </button>
-      </Form>
+      {({ values }) => (
+        <Form className={s.Form}>
+          <label className={s.labelForm}>
+            <span>Ім'я</span>
+            <Field className={s.Field} name="contactName"></Field>
+          </label>
+          <label className={s.labelForm}>
+            <span>Телефон</span>
+            <Field className={s.Field} name="contactPhoneNumber"></Field>
+          </label>
+          <label className={s.labelForm}>
+            <span>Город</span>
+            <Field className={s.Field} name="contactCity"></Field>
+          </label>
+          <label className={s.labelForm}>
+            <span>Вид контакту</span>
+            <Field as="select" className={s.Field} name="contactType">
+              <option value="Персональний">Персональний</option>
+              <option value="Новий">Новий</option>
+              <option value="Робочий">Робочий</option>
+            </Field>
+          </label>
+          <div className={s.radioGender}>
+            <label>
+              <Field
+                type="radio"
+                value="Чоловічий"
+                className={s.Field}
+                name="contactGender"
+              />
+              Чоловічий
+            </label>
+            <label>
+              <Field
+                type="radio"
+                value="Жіночий"
+                className={s.Field}
+                name="contactGender"
+              />
+              Жіночий
+            </label>
+          </div>
+          <label className={s.agreementLabel}>
+            <Field type="checkbox" name="contactAgreement" />
+            Прочитав правила та погоджуюсь!
+          </label>
+          <button
+            disabled={values.contactAgreement === false}
+            type="submit"
+            className={s.ButtonAdd}
+          >
+            Add
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
