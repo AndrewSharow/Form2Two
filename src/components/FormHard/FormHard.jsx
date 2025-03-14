a8e13cd6ec74af14d52eb027975312c46ced3ac7
Andrew <psixkrichit@gmail.com>
2025-03-14 02:19:13 +0200

    all works. tODO: validation small and adding contact

--- src/components/FormHard/FormHard.jsx
+++ src/components/FormHard/FormHard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import s from "./FormHard.module.css";
+import * as Yup from "yup";
 
 const FormHard = () => {
   const initialValues = {
@@ -11,25 +12,40 @@ const FormHard = () => {
     contactGender: "",
     contactAgreement: false,
   };
+
   const handleSubmit = (values, options) => {
     console.log("values", values);
     options.resetForm();
   };
+
+  const orderSchema = Yup.object().shape({
+    contactName: Yup.string().min(2).max(24).required(),
+  });
+
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={orderSchema}
+    >
       {({ values }) => (
         <Form className={s.Form}>
           <label className={s.labelForm}>
             <span>Ім'я</span>
-            <Field className={s.Field} name="contactName"></Field>
+            <Field className={s.Field} name="contactName" />
+            <ErrorMessage
+              name="contactName"
+              component="p"
+              className={s.errorMessage}
+            />
           </label>
           <label className={s.labelForm}>
             <span>Телефон</span>
-            <Field className={s.Field} name="contactPhoneNumber"></Field>
+            <Field className={s.Field} name="contactPhoneNumber" />
           </label>
           <label className={s.labelForm}>
             <span>Город</span>
-            <Field className={s.Field} name="contactCity"></Field>
+            <Field className={s.Field} name="contactCity" />
           </label>
           <label className={s.labelForm}>
             <span>Вид контакту</span>
